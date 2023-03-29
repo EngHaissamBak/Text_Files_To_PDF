d869d15cf2ccd5cfb3220cd58ffffe0dcd8cf677
HaithamBk <engineerhmb007@gmail.com>
2023-03-29 15:02:21 +0200

    - Reading text data files and putting them in pdf using context manager and multi_cell

--- main25.py
+++ main25.py
@@ -12,11 +12,9 @@ from pathlib import Path
 # create an object fpdf from Class FPDF
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
-
 # get the file paths of all the text files and store it in a variable
 filepaths = glob.glob("txt/*.txt")
 print(filepaths)
-
 
 for filepath in filepaths:
     pdf.add_page()  # add a page in pdf file
@@ -27,6 +25,13 @@ for filepath in filepaths:
     pdf.set_font(family="Times", style="B", size=18)  # set the font settings
     pdf.set_text_color(0,0,0)  # set the color settings
     pdf.cell(w=50, h=14 , align="L",txt=name,ln=1 , border=0)
+
+    # get/read the data from each of the text files and store them in a variable using context text manager
+    with open(filepath, "r") as file:
+        content = file.read()
+    pdf.set_font(family="Times", style="I", size=12)  # set the font settings
+    pdf.set_text_color(0, 0, 0)  # set the color settings
+    pdf.multi_cell(w=0,h=8,txt=content)
 
 # generate pdf file (the pdf file should contain 4 pages , each file name as title on  a separate page)
 pdf.output("PDF/animals.pdf")
